84a5e61f749d84fbaf88b156d0363af32a62cdf0
Amir Asghari <amir0011.dragonil@gmail.com>
2026-09-19 09:53:54 +0330

    add player movement

--- src/entities/player/player.rs
+++ src/entities/player/player.rs
@@ -1,4 +1,7 @@
-use avian2d::{collision::collider::Collider, dynamics::rigid_body::RigidBody};
+use avian2d::{
+    collision::collider::Collider,
+    dynamics::rigid_body::{LinearVelocity, LockedAxes, RigidBody},
+};
 use bevy::prelude::*;
 
 #[derive(Component)]
@@ -9,11 +12,12 @@ pub struct PlayerState {
     pub health: u32,
     pub stamina: u32,
     pub velocity: f32,
-    pub hideTime: Timer,
+    pub hide_time: Timer,
 }
 
 pub fn player_plugin(app: &mut App) {
-    app.add_systems(Startup, player_setup);
+    app.add_systems(Startup, player_setup)
+        .add_systems(Update, player_movment);
 }
 
 // const X_EXTENT: f32 = 1000.;
@@ -26,21 +30,45 @@ pub fn player_setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     //  mut res: ResMut<crate::app_state::LoadAssets>,
 ) {
-    let shape = meshes.add(Rectangle::new(50.0, 100.0).to_ring(THICKNESS));
+    let shape = meshes.add(Rectangle::new(25.0, 25.0).to_ring(THICKNESS));
 
     commands.spawn((
         Player,
         PlayerState {
             health: 100,
             stamina: 60,
-            hideTime: Timer::from_seconds(3.0, TimerMode::Once),
-            velocity: 50.0,
+            hide_time: Timer::from_seconds(3.0, TimerMode::Once),
+            velocity: 5000.0,
         },
         RigidBody::Dynamic,
-        Collider::capsule(12.0, 50.0),
+        Collider::rectangle(25.0, 25.0),
         Transform::from_xyz(0.0, 0.0, 1.0),
         // Sprite::from_color(Color::srgb(0.1, 0.6, 0.3), Vec2 { x: 50.0, y: 50.0 }),
         Mesh2d(shape),
+        LockedAxes::ROTATION_LOCKED,
         MeshMaterial2d(materials.add(Color::srgb(0.1, 0.8, 0.1))),
     ));
+}
+
+fn player_movment(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<(&mut LinearVelocity, &PlayerState), With<Player>>,
+) {
+    for (mut velocity, state) in &mut query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            velocity.x = state.velocity * time.delta_secs();
+        } else if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            velocity.x = -state.velocity * time.delta_secs();
+        } else {
+            velocity.x = 0.0; // توقف در صورت رها کردن کلید
+        }
+
+        if velocity.y.abs() < 0.1
+            && !keyboard_input.pressed(KeyCode::ArrowDown)
+            && keyboard_input.just_pressed(KeyCode::ArrowUp)
+        {
+            velocity.y = state.velocity * time.delta_secs();
+        }
+    }
 }
--- src/entities/world/world_setup.rs
+++ src/entities/world/world_setup.rs
@@ -1,13 +1,20 @@
+use avian2d::{collision::collider::Collider, dynamics::rigid_body::RigidBody};
 use bevy::prelude::*;
 
 pub fn world_plugin(app: &mut App) {
     app.add_systems(Startup, world_setup);
 }
 
-fn world_setup(mut commands: Commands, mut res: ResMut<crate::app_state::LoadAssets>) {
+fn world_setup(mut commands: Commands) {
     commands.spawn((Camera2d::default(),));
     commands.spawn((Sprite::from_color(
         Color::srgb(0.1, 0.2, 0.3),
         Vec2 { x: 600.0, y: 800.0 },
     ),));
+    commands.spawn((
+        Sprite::from_color(Color::srgb(0.6, 0.2, 0.6), Vec2 { x: 600.0, y: 10.0 }),
+        Transform::from_xyz(0.0, -300.0, 0.0),
+        RigidBody::Static,
+        Collider::rectangle(600.0, 10.0),
+    ));
 }
